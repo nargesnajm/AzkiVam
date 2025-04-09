ca1600dd7fd8328f59b74a45ee2e843180b99bf4
nargesnajm <nargesnajm@gmail.com>
2025-04-09 11:32:41 +0330

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import ProductList from "@/components/ProductList.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/products",
+  },
+  // Explain: This route is used to display the product list and filters
+  {
+    path: "/products",
+    name: "products",
+    component: ProductList,
+  },
+  // Explain: This route is used to display the product list for a specific category and subcategory.
+  // and it's used for filtering products by category and subcategory.
+  {
+    path: "/products/:categoryId/:subcategoryId",
+    name: "product-list",
+    component: ProductList,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
